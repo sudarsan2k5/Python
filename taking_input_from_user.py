8cffed844b6d28d9a28f24fc842c00f6bdbd0249
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2022-11-22 12:12:29 +0530

    my first code

---
+++ taking_input_from_user.py
@@ -0,0 +1,8 @@
+username = input("Enter Your Username: ")
+password = input("Enter Your Password: ")
+
+if username == "durga" and password == "sunney":
+    print("Valid user")
+
+else:
+    print("Invalid user")
