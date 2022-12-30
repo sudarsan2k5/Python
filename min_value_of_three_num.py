cede1c20f0a96cf3c515c0523433e152c644e42f
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2022-12-30 12:26:18 +0530

    delete all file

--- min_value_of_three_num.py
+++
@@ -1,7 +0,0 @@
-num1 = int(input("Enter Your First Num: "))
-num2 = int(input("Enter Your Second Num: "))
-num3 = int(input("Enter Your Third Num: "))
-
-min = num1 if num1 < num2 and num1 < num3 else num2 if num2 < num3 else num3
-
-print("The Min Value Is: ", min)
--- taking_input_from_user.py
+++
@@ -1,8 +0,0 @@
-username = input("Enter Your Username: ")
-password = input("Enter Your Password: ")
-
-if username == "durga" and password == "sunney":
-    print("Valid user")
-
-else:
-    print("Invalid user")
